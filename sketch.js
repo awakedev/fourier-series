7154b9e7fc8acffe184e5490c2593c786cb0f7dd
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-27 17:28:35 +0000

    wave creation

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
+let time = 0;
+let wave = [];
 
 
 function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(255, 0, 200);
-      }
+    createCanvas(windowWidth, windowHeight);
+}
 
 
 function draw() {
     background(0);
+    translate(200, 200);
+    let radius = 100;
 
-        }
+    stroke(255);
+    noFill();
+    ellipse(0, 0, radius * 2);
+
+    let x = radius * cos(time);
+    let y = radius * sin(time);
+    wave.unshift(y);
+    fill(255);
+    line(0, 0, x, y);
+    ellipse(x, y, 8);
+
+    translate(200, 0);
+    line(x - 200, y, 0, wave[0]);
+
+    beginShape();
+    noFill();
+
+    for (let i = 0; i < wave.length; i++) {
+        vertex(i, wave[i]);
+    }
+    endShape();
+
+    time += 0.05;
+
+    if (wave.length > 50000) {
+        wave.pop();
+    }
+}
